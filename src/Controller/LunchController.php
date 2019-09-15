244d73fe5aa240594f4dad53d2db2434e6871033
Nathan Dunn <nathanstephendunn@gmail.com>
2019-09-15 22:24:53 +1000

    Add route and stub controller for /lunch

---
+++ src/Controller/LunchController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LunchController
+{
+    public function get()
+    {
+        $lunch = [];
+
+        return new Response(
+            json_encode($lunch)
+        );
+    }
+}
