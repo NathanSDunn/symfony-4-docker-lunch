f55e8d5118d7b492b2e10d86249933e23a67384b
Nathan Dunn <nathanstephendunn@gmail.com>
2019-09-18 03:17:46 +1000

    Use Symfony 4 Dependency injection for the /lunch endpoint
    Hurrah! Since our dependencies have now been designed to receive their
    state in the constructor we can use Symfony 4's Dependency Injection
    functionality in order to have much cleaner code.

--- src/Controller/LunchController.php
+++ src/Controller/LunchController.php
@@ -3,19 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use App\Service;
+use App\Service\RecipeService;
 
 class LunchController
 {
-    public function get()
+    /**
+     * This controller manages the /lunch endpoint which will display a JSON formatted list of recipies that contain
+     * Ingredients before their use by date. Recipies containing ingredients that are before their use-by date, but
+     * after their best-before date will be sorted to the bottom of the list.
+     *
+     * @param RecipeService $recipeService
+     * @return Response a json formatted response with the desired lunch options
+     */
+    public function get(RecipeService $recipeService)
     {
-        $recipeService = new Service\RecipeService(
-            new Service\Recipes(),
-            new Service\IngredientService(
-                new Service\Ingredients()
-            )
-        );
-
         return new Response(
             json_encode(
                 ['recipes' => $recipeService->getLunch()]
